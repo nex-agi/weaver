924ff19b71fce771687fa6d0c17c89efcde08628
tang-t21 <tiantang673@gmail.com>
2026-01-09 10:04:22 +0000

    fix annotation

--- weaver/types/lora_config.py
+++ weaver/types/lora_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Training configuration helpers."""
+"""LoRA configuration helpers."""
 
 from __future__ import annotations
 
